4c2f3baddc4a05390a52c2f4fa42391948492902
Qwopfear <Seva552.seva@gmail.com>
2022-01-04 16:10:34 +0100

    Second option for creating a Thread

---
+++ src/Ex2.java
@@ -0,0 +1,26 @@
+public class Ex2 {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new MyThread3());
+        Thread t2 = new Thread(new MyThread4());
+
+        t1.start();
+        t2.start();
+    }
+}
+
+
+class MyThread3 implements Runnable{
+    public void run(){
+        for (int i = 0; i <= 1000; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+class MyThread4 implements Runnable{
+    public void run(){
+        for (int i = 1000; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
