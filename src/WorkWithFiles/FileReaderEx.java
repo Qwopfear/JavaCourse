8043dc4716d7f08ff2c0889efc370f9515b63642
Qwopfear <Seva552.seva@gmail.com>
2022-01-17 16:02:51 +0100

    FileReader Ex

---
+++ src/WorkWithFiles/FileReaderEx.java
@@ -0,0 +1,29 @@
+package WorkWithFiles;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderEx {
+    public static void main(String[] args) throws IOException {
+
+
+        FileReader reader = null;
+
+        try {
+            reader = new FileReader(FileWriterEx.link);
+            int character;
+            while ((character = reader.read()) != -1){
+                System.out.print((char) character);
+            }
+
+            System.out.println();
+            System.out.println("Done");
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            reader.close();
+        }
+    }
+}
