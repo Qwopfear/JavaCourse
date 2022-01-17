6ca909639617c75ffd0c014e16e4e48b26d6bb15
Qwopfear <Seva552.seva@gmail.com>
2022-01-17 15:47:51 +0100

    FileWriter Ex2

--- src/WorkWithFiles/FileWriterEx.java
+++ src/WorkWithFiles/FileWriterEx.java
@@ -6,12 +6,12 @@ import java.io.IOException;
 
 public class FileWriterEx {
     public static void main(String[] args) throws IOException {
-        String link = "/home/vsevolod/Desktop/rubai.txt";
+        String link = "/home/vsevolod/Desktopr/rubai.txt";
         String rubai =
-                "У попа была собака."+
-                "Он ее любил." +
-                "Она сьела кусок мяса." +
-                "Он ее убил.";
+                "У попа была собака.\n"+
+                "Он ее любил.\n" +
+                "Она сьела кусок мяса.\n" +
+                "Он ее убил.\n";
 
 
         FileWriter writer = null;
@@ -22,7 +22,12 @@ public class FileWriterEx {
             }
             System.out.println("Done!");
         }catch (IOException e){
-            e.printStackTrace();
+            writer = new FileWriter("src/WorkWithFiles/rubai2.txt");
+            for (int i = 0; i < rubai.length(); i++) {
+                writer.write(rubai.charAt(i));
+            }
+            writer.write(e.getMessage());
+
         }finally {
             writer.close();
         }
