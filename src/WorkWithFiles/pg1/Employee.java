ecfd9421108a78edd29dc873eee1e5aa4e971838
Qwopfear <Seva552.seva@gmail.com>
2022-01-21 00:00:00 +0100

    Serializable part 2

--- src/WorkWithFiles/pg1/Employee.java
+++ src/WorkWithFiles/pg1/Employee.java
@@ -3,13 +3,18 @@ package WorkWithFiles.pg1;
 import java.io.Serializable;
 
 public class Employee implements Serializable {
+    static final long serialVersionUID = 2;
     String name;
-    int salary;
+    String surname;
+    // if I need to hide information and don`t push it in file
+    // use "transient" key-word
+    transient int salary;
     int age;
     String department;
 
-    public Employee(String name, int salary, int age, String department) {
+    public Employee(String name, String surname, int salary, int age, String department) {
         this.name = name;
+        this.surname = surname;
         this.salary = salary;
         this.age = age;
         this.department = department;
--- src/WorkWithFiles/pg1/SerializationEx2.java
+++ src/WorkWithFiles/pg1/SerializationEx2.java
@@ -7,7 +7,7 @@ import java.io.ObjectOutputStream;
 
 public class SerializationEx2 {
     public static void main(String[] args) {
-        Employee bestEmployee = new Employee("Seva",5000,20,"IT");
+        Employee bestEmployee = new Employee("Seva","Pupkin",5000,20,"IT");
         try(ObjectOutputStream dop = new ObjectOutputStream(
                 new FileOutputStream("src/WorkWithFiles/pg1/bestEmployee.bin"))){
             dop.writeObject(bestEmployee);
