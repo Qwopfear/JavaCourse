c257d8bf3ff42e32c3a5514210d5d2452a328ec4
Qwopfear <Seva552.seva@gmail.com>
2022-02-02 01:01:36 +0100

    Interwiew prep medium1 correction

--- src/interviewPreparation/Medium1.java
+++ src/interviewPreparation/Medium1.java
@@ -8,51 +8,25 @@ public class Medium1 {
     // every word in String s contain number
     // replace words in String according to numbers
     public static void main(String[] args) {
-        String  s = "is2 w5e  go3od 4but thi1s";
+        String  s = "is2 w5e go3od 4but thi1s";
 
-        System.out.println(stringEnum(s));
+        System.out.println(stringEnum(s.trim()));
     }
 
     static String stringEnum(String s){
-        String[] str;
-
-        int amountOfWords = 0;
-        Pattern p = Pattern.compile("\\w+");
-        Matcher m = p.matcher(s);
-        while (m.find()){
-            amountOfWords++;
-
-        }
-        str = new String[amountOfWords];
-        if (!m.find()){
-            amountOfWords = 0;
-        }
-
-        Matcher m2 = p.matcher(s);
-        while (m2.find()){
-            str[amountOfWords] = m2.group();
-            amountOfWords++;
-        }
-
-
-        String[] str2 = new String[amountOfWords];
-
-        Pattern p1 = Pattern.compile("[0-9]");
-        String tmp;
+        System.out.println(s);
+        String[] str = s.trim().split(" ");
+        StringBuilder result = new StringBuilder();
         for (int i = 0; i < str.length; i++) {
-            Matcher m1 = p1.matcher(str[i]);
-//            System.out.println(str[i] + " in matcher");
-            if (m1.find() && (i != Integer.parseInt(m1.group()))){
-                int j = Integer.parseInt(m1.group()) - 1;
-                str2[j] = str[i];
+            for (String value : str) {
+                if (value.indexOf((char) (i + 1 + '0')) != -1) {
+                    System.out.println(value.indexOf((i + 1 + '0')));
+                    result.append(value).append(" ");
+//                    System.out.println(result);
+
+                }
             }
         }
-
-        String fs = "";
-        for (String s1: str2) {
-            fs += s1 + " ";
-        }
-
-        return  fs;
+        return  String.valueOf(result);
     }
 }
